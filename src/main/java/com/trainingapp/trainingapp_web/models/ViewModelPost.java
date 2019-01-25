a3badc1a6ba7eba6cfbd0c4ec57511a69d3fa738
harpersgeekly <harperryanc@gmail.com>
2019-01-25 15:56:00 -0600

    ViewModelPost has @JsonGetters and all other functionality potentially useful in view

--- src/main/java/com/trainingapp/trainingapp_web/models/ViewModelPost.java
+++ src/main/java/com/trainingapp/trainingapp_web/models/ViewModelPost.java
@@ -1,9 +1,13 @@
 package com.trainingapp.trainingapp_web.models;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class ViewModelPost {
@@ -105,5 +109,76 @@ public class ViewModelPost {
 
     public void setPostVotes(List<ViewModelPostVote> postVotes) {
         this.postVotes = postVotes;
+    }
+
+    @JsonGetter("hoursMinutes")
+    public String hoursMinutes() {
+        return date.format(DateTimeFormatter.ofPattern("h:mm a"));
+    }
+
+    @JsonGetter("formatDate")
+    public String formatDate() {
+        return date.format(DateTimeFormatter.ofPattern("MMM dd, yyyy"));
+    }
+
+    // VOTING LOGIC =============================================================================
+    @JsonGetter("voteCount") // saying that this method is only being used as an attribute in show.html
+    public int voteCount() {
+        return postVotes.stream().mapToInt(ViewModelPostVote::getType).reduce(0, (sum, vote) -> sum + vote);
+        // takes all the votes and adds 1 or -1 (getType). Needs more users in application to vote and see results.
+        // http://www.baeldung.com/java-8-double-colon-operator (::)
+        // stream(), mapToInt(), reduce()
+        // https://docs.oracle.com/javase/8/docs/api/java/util/stream/package-summary.html
+        // A stream represents a sequence of elements and supports different kinds of operations to perform computations upon those elements.
+        // Streams can be created from various data sources, especially collections. Lists and Sets support new methods stream()
+        // mapToInt() returns an IntStream consisting of the results of applying the given function to the elements of this stream.
+        // PostVote::getType will evaluate to a function that invokes getType() directly without any delegation.
+        // There’s a really tiny performance difference due to saving one level of delegation.
+        // reduce() sums the values
+    }
+
+    public ViewModelPostVote getVoteFrom(ViewModelUser user) {
+        for (ViewModelPostVote vote : postVotes) {
+            if (vote.voteBelongsTo(user)) {
+                return vote;
+            }
+        }
+        return null;
+    }
+
+    public void addVote(ViewModelPostVote vote) {
+        postVotes.add(vote);
+    }
+
+    public void removeVote(ViewModelPostVote vote) {
+        postVotes.remove(vote);
+    }
+
+
+
+    // MARKDOWN PARSING FOR VIEW ==============================================================
+
+    public String getHtmlTitle() {
+        Parser parser = Parser.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(parser.parse(title));
+    }
+
+    public String getHtmlLeadImage() {
+        Parser parser = Parser.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(parser.parse(leadImage));
+    }
+
+    public String getHtmlSubtitle() {
+        Parser parser = Parser.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(parser.parse(subtitle));
+    }
+
+    public String getHtmlBody() {
+        Parser parser = Parser.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(parser.parse(body));
     }
 }
